27d4b8a8b0f51307aeb21256ee439c951519d098
Abhishek <amukherjee683@gmail.com>
2024-02-26 18:25:52 +0530

    Coursera_Hackathon

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\2303498\git\Hackathon_CTS\Hackathon_Coursera
+mvn test
---
+++ src/test/java/testCases/TC002_WebDevelopmentPage.java
@@ -0,0 +1,62 @@
+package testCases;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import pageObjects.WebDevelopmentPage;
+import utilityFiles.ScreenShots;
+
+public class TC002_WebDevelopmentPage extends TC001_SearchHomePage {
+	
+	
+	@Test(priority = 2,groups= {"regression","Master"})
+	public void levelSelector() throws InterruptedException {
+		wp = new WebDevelopmentPage(driver);
+//		wp.scrollScreen();
+		wp.selectLevel();
+		logger.info("---//Level Selected//---");
+	}
+	
+	@Test(priority = 3,groups= {"regression","Master"})
+	public void languageSelect() throws InterruptedException {
+		wp = new WebDevelopmentPage(driver);
+		Thread.sleep(4000);
+		wp.selectLanguage();
+		Thread.sleep(2000);
+		logger.info("---//Language Selected//---");
+	}
+	
+	WebDevelopmentPage wp = null;
+	
+	@Test(priority =4,groups= {"regression","Master"})
+	public void Result() throws InterruptedException {
+		wp = new WebDevelopmentPage(driver);
+		Thread.sleep(2000);
+		wp.searchResult();
+	}
+	
+	@Test(priority = 6,groups= {"regression","Master"})
+	public void deatilsFirstCard() throws InterruptedException, IOException {
+		ScreenShots ss = new ScreenShots(driver);
+		wp = new WebDevelopmentPage(driver);
+		wp.FirstNameRatingTime();
+		wp.SecondNameRatingTime();
+		ss.screenshot("Courses");
+//		wp.FirstCardsPage();
+	}
+	
+	
+	
+	@Test(priority = 7,groups= {"regression","Master"})
+	public void Headings() throws InterruptedException {
+		wp = new WebDevelopmentPage(driver);
+
+		wp.clickLogo();
+	}
+	
+
+	
+
+
+}
